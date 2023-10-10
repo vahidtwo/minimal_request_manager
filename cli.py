c0908433c7dae66c17fdf14c79802afe6170a19b
vahidtwo <vahidtwo@gmail.com>
2023-10-10 15:37:26 +0330

    :recycle: reafactor cli with command pattern

--- cli.py
+++ cli.py
@@ -1,5 +1,3 @@
-import time
-
 import questionary
 
 from controler import Controller
@@ -7,74 +5,99 @@ from integration.utils import CLIActions
 from log import logger
 
 
-async def cli(controller: Controller, start_time: time.perf_counter) -> None:
-    is_user_want_end = False
-    providers = controller.providers.values()
-    while is_user_want_end is False:
-        await controller.wait_for_complete()
-        end_time = time.perf_counter()
-        logger.info(f"total execution time is {end_time - start_time} seconds")
-        input("press enter to continue")
-        selected_action = await questionary.select(
-            "What do you want to do?",
-            choices=[action for action in CLIActions],
-        ).ask_async()
-        if selected_action == CLIActions.EXIT:
-            is_user_want_end = True
-            controller.stop()
-        elif selected_action == CLIActions.START_PROVIDER:
-            available_providers = [
-                provider.name
-                for provider in providers
-                if provider.enabled.is_set() is False
-            ]
-            if len(available_providers) == 0:
-                logger.info("no provider is available to start")
+class Command:
+    def __init__(self, controller: Controller):
+        self.controller = controller
+
+    async def execute(self):
+        pass
+
+
+class ExitCommand(Command):
+    async def execute(self):
+        self.controller.stop()
+
+
+class StartProviderCommand(Command):
+    async def execute(self):
+        providers = self.controller.providers.values()
+        available_providers = [
+            provider.name for provider in providers if not provider.enabled.is_set()
+        ]
+        if not available_providers:
+            logger.info("No provider is available to start")
+        else:
             selected_provider_name = await questionary.select(
                 "Which provider do you want to start?",
                 choices=available_providers,
             ).ask_async()
-            provider = controller.providers[selected_provider_name]
+            provider = self.controller.providers[selected_provider_name]
             provider.start()
-        elif selected_action == CLIActions.STOP_PROVIDER:
-            available_providers = [
-                provider.name
-                for provider in providers
-                if provider.enabled.is_set() is True
-            ]
-            if len(available_providers) == 0:
-                logger.info("no provider is available to stop")
+
+
+class StopProviderCommand(Command):
+    async def execute(self):
+        providers = self.controller.providers.values()
+        available_providers = [
+            provider.name for provider in providers if provider.enabled.is_set()
+        ]
+        if not available_providers:
+            logger.info("No provider is available to stop")
+        else:
             selected_provider_name = await questionary.select(
                 "Which provider do you want to stop?",
                 choices=available_providers,
             ).ask_async()
-            provider = controller.providers[selected_provider_name]
-            await provider.stop()
-        elif selected_action == CLIActions.ADD_REQUEST:
-            available_providers = [
-                provider.name
-                for provider in providers
-                if provider.enabled.is_set() is True
-            ]
-            priority = int(
-                await questionary.text("put your priority", default="1").ask_async()
-            )
-            execution_time = int(
-                await questionary.text(
-                    "put your execution time in seconds", default="0"
-                ).ask_async()
-            )
-            selected_provider_name = await questionary.select(
-                "Which provider do you want to use?", choices=available_providers
-            ).ask_async()
+            provider = self.controller.providers[selected_provider_name]
+            provider.stop()
 
-            provider = controller.providers[selected_provider_name]
-            logger.info(
-                f"add request/provider Provider[provider.name] "
-                f"with priority {priority} and execution time {execution_time}"
-            )
-            controller.new_request_received(
-                provider=provider,
-                priority=priority,
-                execution_after=execution_time,
-            )
+
+class AddRequestCommand(Command):
+    async def execute(self):
+        providers = self.controller.providers.values()
+        available_providers = [
+            provider.name for provider in providers if provider.enabled.is_set()
+        ]
+        priority = int(
+            questionary.text("Put your priority", default="1").ask_async().result()
+        )
+        execution_time = int(
+            await questionary.text(
+                "Put your execution time in seconds", default="0"
+            ).ask_async()
+        )
+        selected_provider_name = await questionary.select(
+            "Which provider do you want to use?", choices=available_providers
+        ).ask_async()
+
+        provider = self.controller.providers[selected_provider_name]
+        logger.info(
+            f"Add request/provider Provider[{provider.name}] "
+            f"with priority {priority} and execution time {execution_time}"
+        )
+        self.controller.new_request_received(
+            provider=provider,
+            priority=priority,
+            execution_after=execution_time,
+        )
+
+
+async def cli(controller: Controller):
+    command_mapping = {
+        CLIActions.EXIT: ExitCommand(controller),
+        CLIActions.START_PROVIDER: StartProviderCommand(controller),
+        CLIActions.STOP_PROVIDER: StopProviderCommand(controller),
+        CLIActions.ADD_REQUEST: AddRequestCommand(controller),
+    }
+
+    while True:
+        await controller.wait_for_complete()
+        input("Press Enter to continue")
+        selected_action = await questionary.select(
+            "What do you want to do?",
+            choices=[action for action in CLIActions],
+        ).ask_async()
+
+        if selected_action in command_mapping:
+            command = command_mapping[selected_action]
+            await command.execute()
