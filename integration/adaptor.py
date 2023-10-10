0f5d3bcab6dbd944b239f74a9e401f2012eb3094
vahidtwo <vahidtwo@gmail.com>
2023-10-10 15:12:52 +0330

    :truck: move Request from adaptor to request

--- integration/adaptor.py
+++ integration/adaptor.py
@@ -1,40 +1,9 @@
 import datetime
 import time
 
+from integration.request import Request
 from integration.utils import StatusCode, Response
 from log import logger
-
-
-class Request:
-    def __init__(
-        self,
-        provider: "Provider",
-        priority: int,
-        execution_after: datetime.datetime | int = 0,
-        name: str = "",
-    ):
-        self.name = name
-        self.provider = provider
-        # for use in min-heap we must invert the priority to act as a max-heap
-        self.priority = priority * -1
-        if execution_after is not None:
-            if isinstance(execution_after, datetime.datetime):
-                self.execution_time = execution_after.timestamp()
-            elif isinstance(execution_after, int):
-                self.execution_time = time.time() + execution_after
-
-    def __repr__(self):
-        return (
-            f"Request(name={self.name}, priority={self.priority * -1},"
-            f" execution_time={self.execution_time}, provider={self.provider.name})"
-        )
-
-    def __lt__(self, other: "Request"):
-        return self.priority < other.priority
-
-    @property
-    def is_ready(self) -> bool:
-        return time.time() >= self.execution_time
 
 
 class Provider:
