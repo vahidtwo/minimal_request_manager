ea96524a01ee7cfa88b67da25b35f25da5b643a7
vahidtwo <vahidtwo@gmail.com>
2023-10-10 18:40:34 +0330

    :bug: main run cli

--- request_manager/main.py
+++ request_manager/main.py
@@ -1,9 +1,9 @@
 import asyncio
+import logging
 
-
-async def run_job():
-    pass
-
+from request_manager.log import logger
+from .cli import cli
 
 if __name__ == "__main__":
-    asyncio.run(run_job())
+    logger.setLevel(logging.INFO)
+    asyncio.run(cli())
