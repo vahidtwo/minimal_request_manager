72d45c9d18f1e885aeff01ced726f846a02a1cb5
vahidtwo <vahidtwo@gmail.com>
2023-10-10 15:15:02 +0330

    :recycle: refactor Provider - add stop and start method - rename is_available to wait_for_rate_limit - add pending queue for requests and implement their logic

--- integration/adaptor.py
+++ integration/adaptor.py
@@ -1,5 +1,8 @@
+import asyncio
 import datetime
+import logging
 import time
+from asyncio import PriorityQueue
 
 from integration.request import Request
 from integration.utils import StatusCode, Response
@@ -11,27 +14,93 @@ class Provider:
         self.name = name
         self.rate_limit = rate_limit
         self.last_request_time = 0
-        self.enabled = True
+        self.enabled = asyncio.Event()
+        self.enabled.set()
+        self.queue = PriorityQueue()
+        self.pending_request_queue = PriorityQueue()
 
-    @property
-    def is_available(self) -> bool:
+    async def wait_for_rate_limit(self) -> bool:
         """the logic we must check that we can send a request to this provider"""
-        current_time = time.time()
-        if self.enabled and (current_time - self.last_request_time) >= 1 / self.rate_limit:
-            self.last_request_time = current_time
-            return True
-        return False
+        while True:
+            current_time = time.time()
 
-    async def send_request(self) -> Response:
+            if (current_time - self.last_request_time) >= 1 / self.rate_limit:
+                self.last_request_time = current_time
+                return True
+            else:
+                logger.debug(f"waiting for rate limit...")
+                logger.debug(self)
+
+    async def send_request(self, request: Request) -> Response:
         """the logic of sending request with this provider"""
-        print(f"sending request to provider {self.name}")
-        return Response(status_code=StatusCode.SUCCESS, data={})
+        logger.debug(f"sending request [{request.name}] with provider {self.name}")
+        return Response(status_code=StatusCode.SUCCESS, data={"message": "done"})
+
+    def start(self):
+        self.enabled.set()
+
+    async def check_pending_request(self):
+        if self.pending_request_queue.qsize() > 0:
+            priority, request = await self.pending_request_queue.get()
+            if request.is_ready:
+                logger.info(
+                    f"add pending request[{request.name}] to master queue in provider[{self.name}]"
+                )
+                await self.queue.put((priority, request))
+            else:
+                await self.pending_request_queue.put((priority, request))
+            self.pending_request_queue.task_done()
+
+    async def run(self):
+        """start the provider to send requests"""
+        while True:
+            await self.enabled.wait()
+            await self.wait_for_rate_limit()
+            await self.check_pending_request()
+            request: Request
+            priority, request = await self.queue.get()
+            if request.is_ready is False:
+                logger.info(
+                    f"add request[{request.name}] to pending queue in provider[{self.name}]"
+                )
+                self.pending_request_queue.put_nowait((priority, request))
+                self.queue.task_done()
+                continue
+            result = await self.send_request(request)
+            current_time = datetime.datetime.now().strftime("%H:%M:%S")
+            msg = (
+                "Sent request {} to provider {} with priority {} at {}"
+                " (Execution time: {}) {} request remain"
+            ).format(
+                request.name,
+                request.provider.name,
+                request.priority * -1,
+                current_time,
+                datetime.datetime.fromtimestamp(request.execution_time).strftime(
+                    "%H:%M:%S"
+                ),
+                self.queue.qsize(),
+            )
+            logger.info("{}\n| {} |\n{}".format("+" * 100, msg, "+" * 100))
+            if result.status_code != StatusCode.SUCCESS:
+                if request.retry_count >= 3:
+                    logging.error(
+                        f"{request} in provider {self.name} has been retried 3 times"
+                    )
+                    self.queue.task_done()
+                    continue
+                request.retry_count += 1
+                await self.queue.put((priority, request))
+            self.queue.task_done()
+
+    async def stop(self):
+        self.enabled.clear()
 
     def __repr__(self):
-        last_request_time_formated = datetime.datetime.fromtimestamp(self.last_request_time).strftime(
-            "%Y-%m-%d %H:%M:%S"
-        )
+        last_request_time_formated = datetime.datetime.fromtimestamp(
+            self.last_request_time
+        ).strftime("%Y-%m-%d %H:%M:%S")
         return (
             f"Provider(name={self.name}, rate_limit={self.rate_limit}/s,"
-            f" last_request_time={last_request_time_formated}, enabled={self.enabled})"
+            f" last_request_time={last_request_time_formated}, enabled={self.enabled.is_set()})"
         )
--- integration/contract.py
+++ integration/contract.py
@@ -1,19 +1,34 @@
+import asyncio
 from asyncio import PriorityQueue
 from typing import Protocol
 
+from integration.adaptor import Request
 from integration.utils import Response
 
 
 class Provider(Protocol):
-    def __init__(self, name: str, rate_limit: float):
+    def __init__(self, name, rate_limit):
         self.name = name
         self.rate_limit = rate_limit
         self.last_request_time = 0
-        self.enabled = True
+        self.enabled = asyncio.Event()
+        self.enabled.set()
+        self.queue = PriorityQueue()
 
-    @property
-    def is_available(self) -> bool:
+    async def wait_for_rate_limit(self) -> bool:
         """the logic we must check that we can send a request to this provider"""
 
-    async def send_request(self) -> Response:
+    async def send_request(self, request: Request) -> Response:
         """the logic of sending request with this provider"""
+
+    def start(self):
+        pass
+
+    async def check_pending_request(self):
+        pass
+
+    async def run(self):
+        """start the provider to send requests"""
+
+    async def stop(self):
+        pass
