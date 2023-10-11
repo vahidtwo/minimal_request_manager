770db26a428d5ac91ba9932b11c8e42230e45248
vahidtwo <vahidtwo@gmail.com>
2023-10-11 12:07:45 +0330

    :bug: fix project script

--- setup.py
+++ setup.py
@@ -1,11 +1,3 @@
 from setuptools import setup
 
-setup(
-    name="reqeust_manager",
-    version="0.0.1",
-    entry_points={
-        "console_scripts": [
-            "rmcli=request_manager.cli:main",
-        ],
-    },
-)
+setup()
