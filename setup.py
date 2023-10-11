81f7a1b81472d20a856fac129311152ee034e516
vahidtwo <vahidtwo@gmail.com>
2023-10-11 12:36:35 +0330

    :bug: fix version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="request_manager",
-    version="1.0.0",
+    version="0.0.1",
     packages=find_packages(),
     install_requires=["questionary==2.0.1"],
 )
